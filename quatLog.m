d1b69e225aa7f8700ee35c1384e8b22dbd00a256
gaodong2 <42759953+gaodong2@users.noreply.github.com>
2025-03-07 18:18:13 +0800

    Delete quatLog.m

--- quatLog.m
+++
@@ -1,9 +0,0 @@
-function ln_q = quatLog(q)
-    % 四元数的对数映射
-    theta = acos(q(1));
-    if abs(theta) < eps % 如果 theta 接近 0
-        ln_q = [0, 0, 0, 0]; % 返回零向量
-    else
-        ln_q = [0, q(2:4)] * (theta / sin(theta));
-    end
-end
